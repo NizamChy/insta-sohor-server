5c1f47fc71d9de6a0206b45f87191ff3137400d1
NizamChy <nizam.chy.cs@gmail.com>
2023-11-08 15:24:42 +0600

    single post detail can get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,13 @@ async function run() {
     app.get('/allposts', async(req, res) => {
       const cursor = postsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/allposts/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await postsCollection.findOne(query);
       res.send(result);
     })
 
