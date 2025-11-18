f507d1f9949327f01413de2589ed80203a15724d
NizamChy <nizam.chy.cs@gmail.com>
2025-11-18 15:52:36 +0600

    fix cors and delete option

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Backend mongodb index.jsx file 
+// Backend mongodb index.jsx file
 
 const express = require("express");
 const cors = require("cors");
@@ -7,13 +7,33 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//midleware
-app.use(cors());
+// ---------------------
+// CORS MIDDLEWARE FIX
+// ---------------------
+app.use(
+  cors({
+    origin: [
+      "https://insta-sohor.web.app",
+      "https://insta-sohor.firebaseapp.com",
+      "*",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
 
+// ---------------------
+// MONGODB CONNECTION
+// ---------------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9s9fb7y.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,41 +44,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     const wishlistCollection = client.db("wishlistDB").collection("wishlist");
 
     const commentCollection = client.db("commentDB").collection("comment");
 
+    const postsCollection = client.db("blogPostsDB").collection("allposts");
+
+    // ---------------------
+    // WISHLIST ROUTES
+    // ---------------------
+
     app.get("/wishlist", async (req, res) => {
-      const cursor = wishlistCollection.find();
-      const result = await cursor.toArray();
+      const result = await wishlistCollection.find().toArray();
       res.send(result);
     });
 
     app.post("/wishlist", async (req, res) => {
-      const wishlistItem = req.body;
-      const result = await wishlistCollection.insertOne(wishlistItem);
+      const result = await wishlistCollection.insertOne(req.body);
       res.send(result);
     });
 
     app.delete("/wishlist/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: id };
+      const query = { _id: new ObjectId(id) };
       const result = await wishlistCollection.deleteOne(query);
       res.send(result);
     });
 
-    const postsCollection = client.db("blogPostsDB").collection("allposts");
+    // ---------------------
+    // BLOG POSTS ROUTES
+    // ---------------------
 
     app.get("/allposts", async (req, res) => {
-      const cursor = postsCollection.find();
-      const result = await cursor.toArray();
+      const result = await postsCollection.find().toArray();
       res.send(result);
     });
 
-    // Single post data
     app.get("/allposts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -66,38 +89,24 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/comment", async (req, res) => {
-      const cursor = commentCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    app.post("/comment", async (req, res) => {
-      const newComment = req.body;
-      const result = await commentCollection.insertOne(newComment);
+    app.post("/allposts", async (req, res) => {
+      const result = await postsCollection.insertOne(req.body);
       res.send(result);
     });
-
-    app.delete("/comment/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await commentCollection.deleteOne(query);
-      res.send(result);
-    })
 
     app.delete("/allposts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await postsCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     app.put("/allposts/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-
       const updatedPost = req.body;
+
       const post = {
         $set: {
           name: updatedPost.name,
@@ -116,31 +125,45 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/allposts", async (req, res) => {
-      const newPost = req.body;
-      console.log(newPost);
-      const result = await postsCollection.insertOne(newPost);
+    // ---------------------
+    // COMMENTS ROUTES
+    // ---------------------
+
+    app.get("/comment", async (req, res) => {
+      const result = await commentCollection.find().toArray();
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    app.post("/comment", async (req, res) => {
+      const result = await commentCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    app.delete("/comment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await commentCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    console.log("MongoDB connection successful!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    // keep connection alive for server
   }
 }
+
 run().catch(console.dir);
 
-// -------------------------------
-
+// ---------------------
+// ROOT ROUTE
+// ---------------------
 app.get("/", (req, res) => {
   res.send("Insta Sohor server is running");
 });
 
+// ---------------------
+// START SERVER
+// ---------------------
 app.listen(port, () => {
   console.log(`Insta Sohor server is running on port: ${port}`);
 });
